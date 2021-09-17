968f925cdcbe2018fa61510d2d5a15236483692f
Lucas Silva <lucassilva_199706@hotmal.com>
2021-09-17 13:26:41 -0300

    Projeto

---
+++ Tabuada.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+    int main ()  {
+
+        cout << "Insira um valor: " ;
+
+        int n = 0;
+        cin >> n;
+
+        for (int i = 1; i<=10; i++) {
+    
+        cout << "O  resultado e: " <<  n << " x " << 1 << " = " << i*n << endl;
+
+    }
+
+    return 0;
+
+}
